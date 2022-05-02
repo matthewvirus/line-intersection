348196ebd7eca76b2238fe79c3a4c30dab2ff31d
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-05-02 16:14:01 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,21 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
-
+    int k, b;
     public Line(int k, int b) {
-
+        this.k = k;
+        this.b = b;
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        if (k == other.k) {
+            return null;
+        } else {
+            int x, y;
+            x = (other.b - b)/(k - other.k);
+            y = k * x + b;
+            return new Point(x, y);
+        }
     }
 
 }
